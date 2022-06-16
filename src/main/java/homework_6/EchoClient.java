7648852cd7c5f9d066b55e6ab7ee1438b2bb2f26
Vasily Kazarezov <vasily.kazarezov@cgr-tech.com>
2022-06-16 19:32:18 +0200

    HomeWork 6

---
+++ src/main/java/homework_6/EchoClient.java
@@ -0,0 +1,113 @@
+package homework_6;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoClient {
+
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    private Thread thread;
+
+    public static void main(String[] args) {
+        new EchoClient().start();
+    }
+
+    private void start() {
+
+        try {
+            openConnection();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        Thread enterTextThread = new Thread(() -> {
+
+            while (true) {
+                Scanner scanner = new Scanner(System.in);
+                String a = scanner.nextLine();
+                SendMessage(a);
+
+                if (a.equals("/end")) {
+                    thread.stop();
+                    break;
+                }
+            }
+        });
+
+        enterTextThread.setDaemon(true);
+        enterTextThread.start();
+
+    }
+
+
+    private void SendMessage(String message) {
+
+        try {
+            out.writeUTF(message);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void openConnection() throws IOException {
+
+        socket = new Socket("127.0.0.1", 8189);
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+
+        thread = new Thread(() -> {
+            try {
+                while (true) {
+                    String message = in.readUTF();
+                    if (message.equals("/end")) {
+                        break;
+                    }
+                    System.out.println("Message from server " + message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                CloseConnection();
+            }
+        });
+
+        thread.start();
+
+    }
+
+    private void CloseConnection() {
+
+
+        if (in != null) {
+            try {
+                in.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            try {
+                socket.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
+    }
+}
+
---
+++ src/main/java/homework_6/EchoServer.java
@@ -0,0 +1,110 @@
+package homework_6;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoServer {
+
+    private static Socket socket;
+    private static DataInputStream in;
+    private static DataOutputStream out;
+
+    static Thread thread1;
+
+    public static void main(String[] args) {
+
+        try (ServerSocket serverSocket = new ServerSocket(8189)) {
+
+            System.out.println("Server is running. Waiting for client response... ");
+            socket = serverSocket.accept();
+            System.out.println("Client is connected!");
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+
+            thread1 = new Thread(() -> {
+                try {
+                    while (true) {
+
+                        String str = in.readUTF();
+                        if (str.equals("/end")) {
+                            break;
+                        }
+                        System.out.println("Message from client " + str);
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    CloseConnection();
+                }
+            });
+
+            thread1.start();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        Thread enterTextThread = new Thread(() -> {
+
+            while (true) {
+                Scanner scanner = new Scanner(System.in);
+                String a = scanner.nextLine();
+                SendMessage(a);
+                if (a.equals("/end")) {
+                    thread1.stop();
+                    break;
+                }
+            }
+
+        });
+
+        enterTextThread.setDaemon(true);
+        enterTextThread.start();
+
+    }
+
+
+
+    private static void SendMessage(String message) {
+
+        try {
+            out.writeUTF(message);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static void CloseConnection() {
+
+        if (in != null) {
+            try {
+                in.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
